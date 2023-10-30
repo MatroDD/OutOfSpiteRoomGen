ccf7e00e696da0e8490f4ad843f6c1f42f26a4dd
Saverix339 <savi.difra@gmail.com>
2023-10-30 18:47:54 +0100

    Update outofspitegenerator_2.py

--- outofspitegenerator_2.py
+++ outofspitegenerator_2.py
@@ -7,21 +7,25 @@ generatednames = []
 #occupied_coordinates = []
 d = []
 connections = []
-def change_cords(coordinates, generated_directions):
+def change_cords(coordinates, generated_directions, not_go_here):
     result = randint(1,4)
-    while result in generated_directions:
+    while result in generated_directions or result==not_go_here:
         result = randint(1,4)
     new_coordinates = coordinates.copy()
     if result == 1: #north
         new_coordinates[1] +=1
+        not_go_here = 3
     if result == 2: #east
         new_coordinates[0] +=1
+        not_go_here = 4
     if result == 3: #south
         new_coordinates[1] -=1
+        not_go_here = 1
     if result == 4: #west
         new_coordinates[0] -=1
+        not_go_here = 2
     generated_directions.append(result)
-    return new_coordinates
+    return new_coordinates, not_go_here
 
 def run_complexity():
     room_complexity1 = randint(1,20)
@@ -42,7 +46,7 @@ def run_room_name():
     while roomnametest in generatednames:
         addition += 1
         roomnametest = str(roomname)+str(addition)
-    roomname = str(roomname)+str(addition)
+    roomname = str(roomname)+ " (no." +str(addition+1) + ")"
     generatednames.append(roomname)
     return(roomname)
 
@@ -61,32 +65,34 @@ class Room:
         self.num = num
 
 i = 0
-def roomgenerator(n, current_cords): #n is the number of rooms in the floor
+limit_x, limit_y = 5, 10
+
+def roomgenerator(n, current_cords, previous = None, not_go_here = 1): #n is the number of rooms in the floor
     global i
-    if i>=n:
+    if i>=n or current_cords[0]>=limit_x or current_cords[1]>=limit_y:
         return 0
     for r in d:
-        if r.coordinate == current_cords:
-            print("Connecting "+ str(i) + " and " +str(r.num) + "because it ecountered "+str(r.num))
-            connections[i].append(r.num)
-            connections[r.num].append(i)
+        if r.coordinate == current_cords and type(previous)==Room:
+            print("Connecting "+ str(previous.num) + " and " + str(r.num) + " because it ecountered " + str(r.num))
+            connections[previous.num].append(r.num)
+            connections[r.num].append(previous.num)
             return 0
     x = Room(name=run_room_name(), doors=run_doors(), complexity=run_complexity(), adject=run_room_adjective(), coordinate= current_cords, num= i)
     d.append(x)
     i+=1
     generated_directions = []
     for y in range(x.doors):
-        res = roomgenerator(n, change_cords(current_cords, generated_directions))
+        new_cords, not_go_here = change_cords(current_cords, generated_directions, not_go_here)
+        res = roomgenerator(n, new_cords, x, not_go_here)
         if(type(res) == Room): #if it succesfully creates a room
-            print("Connecting "+ str(res.num) + " and " +str(x.num))
+            print("Created "+ str(res.num) + " and connecting it to " +str(x.num))
             connections[res.num].append(x.num)
             connections[x.num].append(res.num)
-    return x
-    
-    
-    
+    return x    
 
 number = int(input("Select a number of rooms "))
+limit_x = int(input("Select a x limit "))
+limit_y = int(input("Select a y limit "))
 for n in range(number):
     connections.append([])
 
@@ -95,5 +101,11 @@ roomgenerator(number, [0, 0])
 print("Successfully generated " + str(len(d)) + " rooms.")
 print("Here is the array of connections: ")
 print(connections)
+
+print("here's a random room: ")
+x = d[randint(1, number)]
+print(str(x.adjective) + " " + str(x.name))
+print("Room no. " + str(x.num))
+    
 
 
